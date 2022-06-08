c1a288153f03832f6dc3b238f8e645e12e9df325
Alexander Radyushin <a.radyushin@pm.me>
2022-06-07 22:57:07 -0300

    fix: avoid ts-types error for engine.io module

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,4 @@ module.exports = {
   testEnvironment: 'node',
   testRegex: '/test/.*\\.(test|spec)?\\.(ts|tsx)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  extensionsToTreatAsEsm: ['.ts'],
 };
